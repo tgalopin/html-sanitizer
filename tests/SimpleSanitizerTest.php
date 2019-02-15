00b6841b8f123c4a221edf1748b89f40c5966be1
Lars Moelleken <lars@moelleken.org>
2019-02-15 01:44:53 +0100

    added some more tests
    I don't know if the "UTF-7 encoding" && "US-ASCII encoding" needs some attention... maybe not, because the encoding can't be changed by the user via meta tag. All other tests are passed and the result looks good :+1:

--- tests/SimpleSanitizerTest.php
+++ tests/SimpleSanitizerTest.php
@@ -133,6 +133,10 @@ class SimpleSanitizerTest extends AbstractSanitizerTest
                 'Lorem ipsum',
             ],
             [
+                '<iframe srcdoc=\'&lt;meta http-equiv="refresh" content="5;url=(link: https://www.google.com/) google.com " /&gt;&lt;script&gt;alert(document.domain + "\n\n" + document.cookie);</script>\'/>',
+                '',
+            ],
+            [
                 '<img src="/img/example.jpg" alt="Image alternative text" title="Image title" class="foo" />',
                 '',
             ],
@@ -311,14 +315,96 @@ class SimpleSanitizerTest extends AbstractSanitizerTest
                 '',
             ],
             [
+                '<TABLE BACKGROUND="javascript:alert(\'XSS\')">',
+                '',
+            ],
+            [
+                '<title onpropertychange=alert(1)></title><title title=></title>',
+                '',
+            ],
+            [
+                '<img src=x:alert(alt) onerror=eval(src) alt=0>',
+                '',
+            ],
+            [
+                "\n><!-\n<b\n<c d=\"'e><iframe onload=alert(1) src=x>\n<a HREF=\"\">\n",
+                "\n&gt;\n&lt;a HREF&#61;&#34;&#34;&gt;\n",
+            ],
+            [
+                '<embed src="data:text/html;base64,PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg=="></embed>',
+                '',
+            ],
+            [
+                '<div><embed allowscriptaccess=always src=/xss.swf><base href=//l0.cm/</div>',
+                '<div></div>',
+            ],
+            [
+                '"><svg><script>/<@/>alert(1337)</script>',
+                '&#34;&gt;',
+            ],
+            [
                 '<scr<a>ipt>alert(1)</script>',
                 '<a>ipt&gt;alert(1)</a>',
+            ],
+            [
+                '<IMG SRC=&#x6A&#x61&#x76&#x61&#x73&#x63&#x72&#x69&#x70&#x74&#x3A&#x61&#x6C&#x65&#x72&#x74&#x28&#x27&#x58&#x53&#x53&#x27&#x29>',
+                '',
+            ],
+            [
+                '<div onClick="&#38&#35&#49&#48&#54&#38&#35&#57&#55&#38&#35&#49&#49&#56&#38&#35&#57&#55&#38&#35&#49&#49&#53&#38&#35&#57&#57&#38&#35&#49&#49&#52&#38&#35&#49&#48&#53&#38&#35&#49&#49&#50&#38&#35&#49&#49&#54&#38&#35&#53&#56&#38&#35&#57&#57&#38&#35&#49&#49&#49&#38&#35&#49&#49&#48&#38&#35&#49&#48&#50&#38&#35&#49&#48&#53&#38&#35&#49&#49&#52&#38&#35&#49&#48&#57&#38&#35&#52&#48&#38&#35&#52&#57&#38&#35&#52&#49">Clickhere</div>',
+                '<div>Clickhere</div>',
+            ],
+            [
+                '<svg><![CDATA[><image xlink:href="]]><img src=xx:x onerror=alert(2)//"></svg>',
+                '',
+            ],
+            [
+                '<b a=<=" onmouseover="alert(1),1>1">',
+                '<strong></strong>',
+            ],
+            [
+                '<BGSOUND SRC="javascript:alert(\'XSS\');">',
+                '',
+            ],
+            [
+                '<BR SIZE="&{alert(\'XSS\')}">',
+                '<br />',
+            ],
+            [
+                '<META HTTP-EQUIV="Set-Cookie" Content="USERID=&lt;SCRIPT&gt;alert(\'XSS\')&lt;/SCRIPT&gt;">',
+                '',
+            ],
+            // UTF-7 encoding
+            [
+                '<HEAD><META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-7"> </HEAD>+ADw-SCRIPT+AD4-alert(\'XSS\');+ADw-/SCRIPT+AD4-',
+                ' &#43;ADw-SCRIPT&#43;AD4-alert(&#039;XSS&#039;);&#43;ADw-/SCRIPT&#43;AD4-',
+            ],
+            // US-ASCII encoding
+            [
+                '¼script¾alert(¢XSS¢)¼/script¾',
+                '¼script¾alert(¢XSS¢)¼/script¾',
             ],
 
             /*
              * Styles
              */
 
+            [
+                "<DIV STYLE=\"background-image:\\0075\\0072\\006C\\0028\'\\006a\\0061\\0076\\0061\\0073\\0063\\0072\\0069\\0070\\0074\\003a\\0061\\006c\\0065\\0072\\0074\\0028\\0027\\0058\\0053\\0053\\0027\\0029\'\\0029\">",
+                '<div></div>',
+            ],
+            [
+                "<STYLE>.XSS{background-image:url(\"javascript:alert(\'XSS\')\");}</STYLE><A CLASS=XSS></A>",
+                '<a></a>',
+            ],
+            [
+                '<style>*{x:ｅｘｐｒｅｓｓｉｏｎ(write(1))}</style>',
+                '',
+            ],
+            [
+                '<STYLE>@import\'http://ha.ckers.org/xss.css\';</STYLE>',
+                '',
+            ],
             [
                 '<div>Lorem ipsum dolor sit amet, consectetur.<style>body { background: red; }</style></div>',
                 '<div>Lorem ipsum dolor sit amet, consectetur.</div>',
@@ -367,6 +453,207 @@ class SimpleSanitizerTest extends AbstractSanitizerTest
             [
                 '<a title="``onmouseover=alert(1337)">Hello</a>',
                 '<a title="&#96;&#96;onmouseover&#61;alert(1337) ">Hello</a>',
+            ],
+
+            /*
+             * more xss :)
+             */
+
+            // Tag stripping, different ways to work around removal of HTML tags.
+            [
+                '<script>alert(0)</script>',
+                '',
+            ],
+            [
+                '<script src="http://www.example.com" />',
+                '',
+            ],
+            [
+                '<ScRipt sRc=http://www.example.com/>',
+                '',
+            ],
+            [
+                "<script\nsrc\n=\nhttp://www.example.com/\n>",
+                '',
+            ],
+            [
+                '<script/a src=http://www.example.com/a.js></script>',
+                '',
+            ],
+            [
+                '<script/src=http://www.example.com/a.js></script>',
+                '',
+            ],
+            // Null between < and tag name works at least with IE6.
+            [
+                "<\0scr\0ipt>alert(0)</script>",
+                '',
+            ],
+            [
+                '<scrscriptipt src=http://www.example.com/a.js>',
+                '',
+            ],
+            [
+                '<<script>alert(0);//<</script>',
+                '',
+            ],
+            [
+                '<script src=http://www.example.com/a.js?<b>',
+                '',
+            ],
+            // DRUPAL-SA-2008-047: This doesn't seem exploitable, but the filter should
+            // work consistently.
+            [
+                '<script>>',
+                '',
+            ],
+            [
+                '<script src=//www.example.com/.a>',
+                '',
+            ],
+            [
+                '<script src=http://www.example.com/.a',
+                '',
+            ],
+            [
+                '<script src=http://www.example.com/ <',
+                '',
+            ],
+            [
+                '<nosuchtag attribute="newScriptInjectionVector">',
+                '',
+            ],
+            [
+                '<t:set attributeName="innerHTML" to="&lt;script defer&gt;alert(0)&lt;/script&gt;">',
+                '',
+            ],
+            [
+                '<img """><script>alert(0)</script>',
+                '',
+
+            ],
+            [
+                '<blockquote><script>alert(0)</script></blockquote>',
+                '<blockquote></blockquote>',
+            ],
+            [
+                '<!--[if true]><script>alert(0)</script><![endif]-->',
+                '',
+            ],
+            // Dangerous attributes removal.
+            [
+                '<p onmouseover="http://www.example.com/">',
+                '<p></p>',
+            ],
+            [
+                '<li style="list-style-image: url(javascript:alert(0))">',
+                '',
+            ],
+            [
+                '<img onerror   =alert(0)>',
+                '',
+            ],
+            [
+                '<img onabort!#$%&()*~+-_.,:;?@[/|\]^`=alert(0)>',
+                '',
+            ],
+            [
+                '<img oNmediAError=alert(0)>',
+                '',
+            ],
+            // Works at least with IE
+            [
+                "<img o\0nfocus\0=alert(0)>",
+                '',
+            ],
+            // Only whitelisted scheme names allowed in attributes.
+            [
+                '<img src="javascript:alert(0)">',
+                '',
+            ],
+            [
+                '<img src=javascript:alert(0)>',
+                '',
+            ],
+            // A bit like CVE-2006-0070.
+            [
+                '<img src="javascript:confirm(0)">',
+                '',
+            ],
+            [
+                '<img src=`javascript:alert(0)`>',
+                '',
+            ],
+            [
+                '<img dynsrc="javascript:alert(0)">',
+                '',
+            ],
+            [
+                '<table background="javascript:alert(0)">',
+                '',
+            ],
+            [
+                '<base href="javascript:alert(0);//">',
+                '',
+            ],
+            [
+                '<img src="jaVaSCriPt:alert(0)">',
+                '',
+            ],
+            [
+                '<img src=&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;&#97;&#108;&#101;&#114;&#116;&#40;&#48;&#41;>',
+                '',
+            ],
+            [
+                '<img src=&#00000106&#0000097&#00000118&#0000097&#00000115&#0000099&#00000114&#00000105&#00000112&#00000116&#0000058&#0000097&#00000108&#00000101&#00000114&#00000116&#0000040&#0000048&#0000041>',
+                '',
+            ],
+            [
+                '<img src=&#x6A&#x61&#x76&#x61&#x73&#x63&#x72&#x69&#x70&#x74&#x3A&#x61&#x6C&#x65&#x72&#x74&#x28&#x30&#x29>',
+                '',
+            ],
+            [
+                "<img src=\"jav\tascript:alert(0)\">",
+                '',
+            ],
+            [
+                '<img src="jav&#x09;ascript:alert(0)">',
+                '',
+            ],
+            [
+                '<img src="jav&#x000000A;ascript:alert(0)">',
+                '',
+            ],
+            [
+                "<img src=\"\n\n\nj\na\nva\ns\ncript:alert(0)\">",
+                '',
+            ],
+            [
+                "<img src=\"jav\0a\0\0cript:alert(0)\">",
+                '',
+            ],
+            [
+                '<img src="vbscript:msgbox(0)">',
+                '',
+            ],
+            [
+                '<img src="nosuchscheme:notice(0)">',
+                '',
+            ],
+            // Netscape 4.x javascript entities.
+            [
+                '<br size="&{alert(0)}">',
+                '<br />',
+            ],
+            // DRUPAL-SA-2008-006: Invalid UTF-8, these only work as reflected XSS with
+            // Internet Explorer 6.
+            [
+                "<p arg=\"\xe0\">\" style=\"background-image: url(j\xe0avas\xc2\xa0cript:alert(0));\"\xe0<p>",
+                '',
+            ],
+            [
+                '<img src=" &#14;  javascript:alert(0)">',
+                '',
             ],
         ]);
     }
