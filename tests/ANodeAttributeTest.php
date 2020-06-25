c4c4053efb653791c4601e861bf1157279ada59f
Peter <peter@peteruk.net>
2020-06-25 14:29:43 +0100

    Replacing accidental tabs with spaces

--- tests/ANodeAttributeTest.php
+++ tests/ANodeAttributeTest.php
@@ -7,66 +7,66 @@ use PHPUnit\Framework\TestCase;
 
 class ANodeAttributeTest extends TestCase {
 
-	public function testForceRelAttribute()
-	{
-		$sanitizer = Sanitizer::create([
-			'extensions' => ['basic'],
-			'tags' => [
-				'a' => [
-					'force_rel' => 'nofollow noopener'
-				]
-			]
-		]);
+    public function testForceRelAttribute()
+    {
+        $sanitizer = Sanitizer::create([
+            'extensions' => ['basic'],
+            'tags' => [
+                'a' => [
+                    'force_rel' => 'nofollow noopener'
+                ]
+            ]
+        ]);
 
-		$tests = [
-			[
-				'<a href="https://trusted.com" title="Link title">Lorem ipsum</a>',
-				'<a href="https://trusted.com" rel="nofollow noopener" title="Link title">Lorem ipsum</a>'
-			],
-			[
-				'<a href="https://trusted.com" rel="somethinginvalid" title="Link title">Lorem ipsum</a>',
-				'<a href="https://trusted.com" rel="nofollow noopener" title="Link title">Lorem ipsum</a>'
-			],
-			[
-				'<a href="https://trusted.com" rel="nofollow noopener" title="Link title">Lorem ipsum</a>',
-				'<a href="https://trusted.com" rel="nofollow noopener" title="Link title">Lorem ipsum</a>'
-			],
-		];
+        $tests = [
+            [
+                '<a href="https://trusted.com" title="Link title">Lorem ipsum</a>',
+                '<a href="https://trusted.com" rel="nofollow noopener" title="Link title">Lorem ipsum</a>'
+            ],
+            [
+                '<a href="https://trusted.com" rel="somethinginvalid" title="Link title">Lorem ipsum</a>',
+                '<a href="https://trusted.com" rel="nofollow noopener" title="Link title">Lorem ipsum</a>'
+            ],
+            [
+                '<a href="https://trusted.com" rel="nofollow noopener" title="Link title">Lorem ipsum</a>',
+                '<a href="https://trusted.com" rel="nofollow noopener" title="Link title">Lorem ipsum</a>'
+            ],
+        ];
 
-		foreach($tests as $testCase) {
-			$this->assertSame( $testCase[1], $sanitizer->sanitize( $testCase[0] ) );
-		}
-	}
+        foreach($tests as $testCase) {
+            $this->assertSame( $testCase[1], $sanitizer->sanitize( $testCase[0] ) );
+        }
+    }
 
-	public function testForceTargetAttribute()
-	{
-		$sanitizer = Sanitizer::create([
-			'extensions' => ['basic'],
-			'tags' => [
-				'a' => [
-					'force_target' => '_blank'
-				]
-			]
-		]);
+    public function testForceTargetAttribute()
+    {
+        $sanitizer = Sanitizer::create([
+            'extensions' => ['basic'],
+            'tags' => [
+                'a' => [
+                    'force_target' => '_blank'
+                ]
+            ]
+        ]);
 
-		$tests = [
-			[
-				'<a href="https://trusted.com" title="Link title">Lorem ipsum</a>',
-				'<a href="https://trusted.com" target="_blank" title="Link title">Lorem ipsum</a>'
-			],
-			[
-				'<a href="https://trusted.com" target="_self" title="Link title">Lorem ipsum</a>',
-				'<a href="https://trusted.com" target="_blank" title="Link title">Lorem ipsum</a>'
-			],
-			[
-				'<a href="https://trusted.com" target="_blank" title="Link title">Lorem ipsum</a>',
-				'<a href="https://trusted.com" target="_blank" title="Link title">Lorem ipsum</a>'
-			],
-		];
+        $tests = [
+            [
+                '<a href="https://trusted.com" title="Link title">Lorem ipsum</a>',
+                '<a href="https://trusted.com" target="_blank" title="Link title">Lorem ipsum</a>'
+            ],
+            [
+                '<a href="https://trusted.com" target="_self" title="Link title">Lorem ipsum</a>',
+                '<a href="https://trusted.com" target="_blank" title="Link title">Lorem ipsum</a>'
+            ],
+            [
+                '<a href="https://trusted.com" target="_blank" title="Link title">Lorem ipsum</a>',
+                '<a href="https://trusted.com" target="_blank" title="Link title">Lorem ipsum</a>'
+            ],
+        ];
 
-		foreach($tests as $testCase) {
-			$this->assertSame( $testCase[1], $sanitizer->sanitize( $testCase[0] ) );
-		}
-	}
+        foreach($tests as $testCase) {
+            $this->assertSame( $testCase[1], $sanitizer->sanitize( $testCase[0] ) );
+        }
+    }
 
 }
